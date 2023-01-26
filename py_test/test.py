9f4e38dd8db5c3625aff222ce1cb979e5c083321
David Pantelic <dpdavid156@gmail.com>
2023-01-27 00:05:46 +0100

    function for changing text in the file

--- py_test/test.py
+++ py_test/test.py
@@ -1,3 +1,15 @@
 # test
 
-# ovo je pocetak
+def change_file(old, new):
+  with open("py_test/text.txt", 'r') as file:
+    data = file.read()
+
+  data = data.replace(old, new)
+
+  with open("py_test/text.txt", 'w') as file:
+    data = file.write(data)
+    
+
+
+
+change_file('TEST', '____')
